71e3b5d45b410d5a12461a1a89646bbda5317333
nankokuchu <nankokutyu@yahoo.co.jp>
2021-03-27 20:19:37 +0900

    テストコード実装準備

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    emsil {Faker::Internet.free_emal}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    nickname {Faker::Name.last_name}
+    first_name { 田中 }
+    last_name { 大浪 }
+    first_name_reading { タナカ }
+    last_name_reading { ダイロウ }
+    birthday { 1930-01-01 }
+  end
+end
