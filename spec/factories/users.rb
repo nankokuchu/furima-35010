8c6046a540ea37aa4cd6a620f780b022fd9708e3
nankokuchu <nankokutyu@yahoo.co.jp>
2021-03-27 21:04:55 +0900

    Userモデルのテストコード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :user do
-    emsil {Faker::Internet.free_emal}
+    email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
     nickname {Faker::Name.last_name}
-    first_name { 田中 }
-    last_name { 大浪 }
-    first_name_reading { タナカ }
-    last_name_reading { ダイロウ }
-    birthday { 1930-01-01 }
+    first_name { "田中" }
+    last_name { "大浪" }
+    first_name_reading { "タナカ" }
+    last_name_reading { "ダイロウ" }
+    birthday { "1930-01-01" }
   end
 end
