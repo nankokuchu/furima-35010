ec8c67cd15ff5c5c6382ca765c1d9db9d1cdfbfe
nankokuchu <nankokutyu@yahoo.co.jp>
2021-03-27 19:37:32 +0900

    userモデルにvalidates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters."}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters."}
+    # カタカナのみ許可する
+    validates :first_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "Full-width katakana characters"}
+    validates :last_name_reading, format: {with: /\A[ァ-ヶー]+\z/, message: "Full-width katakana characters"}
+    # 半角英字数字のみ許可する
+    validates :nickname, format: {with: /\A[a-z0-9]+\z/i}
+    validates :birthday
+  end
 end
