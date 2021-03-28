9760d98dbd99fdc787c90637efca5e06270da02a
nankokuchu <nankokutyu@yahoo.co.jp>
2021-03-28 14:30:07 +0900

    userモデルバリデーションオプション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,22 @@ class User < ApplicationRecord
 
   with_options presence: true do
     # ひらがな、カタカナ、漢字のみ許可する
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters.' }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters.' } do
+      validates :first_name
+      validates :last_name
+    end
     # カタカナのみ許可する
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width katakana characters' }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width katakana characters' }
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width katakana characters' } do
+      validates :first_name_reading
+      validates :last_name_reading
+    end
     # 半角英字数字のみ許可する
     validates :nickname, format: { with: /\A[a-z0-9]+\z/i }
     validates :birthday
   end
+
+  # パスワードは半角英字と数字のみ許可する
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers' 
+  
 end
