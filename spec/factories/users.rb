0ebeadf3a3859beb811a20260d54d05165466cad
nankokuchu <nankokutyu@yahoo.co.jp>
2021-03-30 22:11:19 +0900

    password　Faker記述修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
-    password { password }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     nickname { Faker::Name.last_name }
     first_name { '田中' }
