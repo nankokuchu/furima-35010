491414962825890d9bc1fc15fb335a768109569b
nankokuchu <nankokutyu@yahoo.co.jp>
2021-03-31 12:04:48 +0900

    userテーブルとitemテーブルにアソシエーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   with_options presence: true do
     # ひらがな、カタカナ、漢字のみ許可する
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters.' } do
