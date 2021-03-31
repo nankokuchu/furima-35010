bbdb4684cb9e678a0c58167e764e972285ce9e26
nankokuchu <nankokutyu@yahoo.co.jp>
2021-03-31 11:35:06 +0900

    itemのindex,new,createアクション定義

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,26 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def new
+    @item =Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+    private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :description, :shipping_cost_id, :prefecture_id,
+    :days_to_ship_id, :items_status_id, :category_id).merge(user_id: current_user.id)
+  end
+  
+  end
