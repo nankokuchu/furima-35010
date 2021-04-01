5ece880fd958473eee10f0dc4e76abcbf3775518
nankokuchu <nankokutyu@yahoo.co.jp>
2021-04-01 17:17:39 +0900

    環境変数に修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', pay);
 
 function pay(){
-  Payjp.setPublicKey("pk_test_5c1f24b6b6f9392224d91faf");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById('charge-form');
   form.addEventListener('submit', (e) =>{
     e.preventDefault()
