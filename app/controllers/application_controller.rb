c9b8aba3f16ffbbe2f1288ba20e9b586bcda984f
nankokuchu <nankokutyu@yahoo.co.jp>
2021-04-01 11:26:09 +0900

    Basic認証及び環境変数設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :confiqure_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
 
   private
 
@@ -9,5 +9,11 @@ class ApplicationController < ActionController::Base
       :sign_up,
       keys: [:nickname, :first_name, :last_name, :first_name_reading, :last_name_reading, :birthday]
     )
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
