f7bed1341118694996de42fa51b9974a4ab620bf
nankokuchu <nankokutyu@yahoo.co.jp>
2021-04-01 16:46:20 +0900

    ルーテング修正（スペルミス）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items do
-    resources :order, only: [:new, :create]
+    resources :orders, only: [:new, :create]
   end
   root to: "items#index"
 end
