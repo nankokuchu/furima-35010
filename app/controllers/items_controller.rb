7fb810dbf1bc3540678635efd11f417069f54683
nankokuchu <nankokutyu@yahoo.co.jp>
2021-04-01 12:40:12 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,12 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
-    @item =Item.new
+    @item = Item.new
   end
 
   def create
@@ -26,9 +25,8 @@ class ItemsController < ApplicationController
 
   private
 
-    def item_params
-      params.require(:item).permit(:name, :image, :price, :description, :shipping_cost_id, :prefecture_id,
-      :days_to_ship_id, :items_status_id, :category_id).merge(user_id: current_user.id)
-    end
-
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :description, :shipping_cost_id, :prefecture_id,
+                                 :days_to_ship_id, :items_status_id, :category_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -15,18 +15,14 @@ class Item < ApplicationRecord
     validates :price
     validates :description
   end
-  
-  with_options numericality: {other_than: 0, message: "can't be blank" } do
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :shipping_cost_id
     validates :items_status_id
     validates :days_to_ship_id
     validates :category_id
     validates :prefecture_id
   end
-  
+
   validates :price, numericality: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
-  
-
 end
-
-  
